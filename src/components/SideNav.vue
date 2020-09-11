cd7f2f246a8e66e038321bbb23ff3012c3c8aac0
Zemin <likeredapple@outlook.com>
2020-09-11 07:35:34 -0700

    added side navigation component

---
+++ src/components/SideNav.vue
@@ -0,0 +1,96 @@
+<template>
+    <kendo-panelbar  expandMode="single">
+        <li>
+            GLOBAL ONE
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>              
+        <li>
+            My Menu
+            <ul>
+                <li>Sub Item 1
+                    <ul>
+                        <li>Sub Item 1.1</li>
+                        <li>Sub Item 1.2</li>
+                        <li>Sub Item 1.3</li>
+                    </ul>
+                </li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>
+        <li>
+            Forwarder
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>
+        <li>
+            Warehouse
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>
+        <li>
+            Settlement
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>
+        <li>
+            Accounting
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>
+        <li>
+            Management Information
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>
+        <li>
+            System Management
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>
+        <li>
+            Customer Service
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>
+        <li>
+            EDI Management
+            <ul>
+                <li>Sub Item 1</li>
+                <li>Sub Item 2</li>
+                <li>Sub Item 3</li>
+            </ul>
+        </li>                                                                                                                                              
+    </kendo-panelbar>        
+</template>
+
+<script>
+export default {
+    name: 'SideNav'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,94 +2,7 @@
   <div class="home" ref="spliterContainer">
       <kendo-splitter style="height:100%" :panes="panes" :orientation="'horizontal'">
         <div>
-            <kendo-panelbar  expandMode="single">
-                <li>
-                    GLOBAL ONE
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>              
-                <li>
-                    My Menu
-                    <ul>
-                        <li>Sub Item 1
-                            <ul>
-                                <li>Sub Item 1.1</li>
-                                <li>Sub Item 1.2</li>
-                                <li>Sub Item 1.3</li>
-                            </ul>
-                        </li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>
-                <li>
-                    Forwarder
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>
-                <li>
-                    Warehouse
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>
-                <li>
-                    Settlement
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>
-                <li>
-                    Accounting
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>
-                <li>
-                    Management Information
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>
-                <li>
-                    System Management
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>
-                <li>
-                    Customer Service
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>
-                <li>
-                    EDI Management
-                    <ul>
-                        <li>Sub Item 1</li>
-                        <li>Sub Item 2</li>
-                        <li>Sub Item 3</li>
-                    </ul>
-                </li>                                                                                                                                              
-            </kendo-panelbar>          
+            <SideNav/>
         </div>
         <div>
             <kendo-tabstrip>
@@ -101,7 +14,7 @@
                 <div>
                     <p>Here Logo Image</p>
                 </div>
-            </kendo-tabstrip>          
+            </kendo-tabstrip>
         </div>
       </kendo-splitter>    
   </div>
@@ -109,13 +22,16 @@
 
 <script>
 // @ is an alias to /src
+import SideNav from '@/components/SideNav.vue'
+
 export default {
   name: 'Home',
   data () {
       return {
           panes: [ { collapsible: true, collapsedSize: "5%", max: "30%", size:"20%", min: "5%", scrollable: false, resizable: true }, {} ]
       }
-  }
+  },
+  components: { SideNav }
 }
 </script>
 
