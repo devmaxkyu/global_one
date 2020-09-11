bca83839b4ddfac5625429d01c60fb252af5532b
Zemin <likeredapple@outlook.com>
2020-09-11 06:39:07 -0700

    updates spliter panes

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import '@progress/kendo-ui'
 import '@progress/kendo-theme-default/dist/all.css'
 import { Calendar } from '@progress/kendo-dateinputs-vue-wrapper'
 import {Button, ButtonsInstaller } from '@progress/kendo-buttons-vue-wrapper'
-import { Splitter, LayoutInstaller } from '@progress/kendo-layout-vue-wrapper'
+import { Splitter, PanelBar, TabStrip , LayoutInstaller } from '@progress/kendo-layout-vue-wrapper'
 
 Vue.config.productionTip = false
 Vue.component(Calendar.name, Calendar)
@@ -16,6 +16,6 @@ new Vue({
   router,  
   render: h => h(App),
   components: {
-    Calendar, Button, Splitter
+    Calendar, Button, Splitter, PanelBar, TabStrip 
   }  
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,108 @@
 <template>
-  <div class="home">
-      <kendo-splitter :panes="panes" :orientation="'horizontal'">
-        <div>Side Navigation</div>
-        <div>Here Content</div>
+  <div class="home" ref="spliterContainer">
+      <kendo-splitter style="height:100%" :panes="panes" :orientation="'horizontal'">
+        <div>
+            <kendo-panelbar  expandMode="single">
+                <li>
+                    GLOBAL ONE
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>              
+                <li>
+                    My Menu
+                    <ul>
+                        <li>Sub Item 1
+                            <ul>
+                                <li>Sub Item 1.1</li>
+                                <li>Sub Item 1.2</li>
+                                <li>Sub Item 1.3</li>
+                            </ul>
+                        </li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>
+                <li>
+                    Forwarder
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>
+                <li>
+                    Warehouse
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>
+                <li>
+                    Settlement
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>
+                <li>
+                    Accounting
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>
+                <li>
+                    Management Information
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>
+                <li>
+                    System Management
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>
+                <li>
+                    Customer Service
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>
+                <li>
+                    EDI Management
+                    <ul>
+                        <li>Sub Item 1</li>
+                        <li>Sub Item 2</li>
+                        <li>Sub Item 3</li>
+                    </ul>
+                </li>                                                                                                                                              
+            </kendo-panelbar>          
+        </div>
+        <div>
+            <kendo-tabstrip>
+                <ul>
+                    <li class="k-state-active">
+                        <span class="k-icon k-i-home"></span>
+                    </li>
+                </ul>
+                <div>
+                    <p>Here Logo Image</p>
+                </div>
+            </kendo-tabstrip>          
+        </div>
       </kendo-splitter>    
   </div>
 </template>
@@ -13,7 +113,7 @@ export default {
   name: 'Home',
   data () {
       return {
-          panes: [ { collapsible: true, collapsedSize: "5%", max: "30%", size:"20%", min: "5%" }, {} ]
+          panes: [ { collapsible: true, collapsedSize: "5%", max: "30%", size:"20%", min: "5%", scrollable: false, resizable: true }, {} ]
       }
   }
 }
